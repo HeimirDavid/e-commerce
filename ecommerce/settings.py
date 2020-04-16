1ceeeda32d71c1d87d5a391e4deda71026ff68c8
HeimirDavid <heimir.97@gmail.com>
2020-04-16 07:10:04 +0200

    done with lessons from 'Products and a Shopping Cart'

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
     'accounts',
+    'products',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +69,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -128,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Fix issue for cloud9, porbably irrelevant here
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
