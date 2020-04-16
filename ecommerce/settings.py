f3f3582271f601df65bce844d697b7b702d76cff
HeimirDavid <heimir.97@gmail.com>
2020-04-16 10:16:12 +0200

    Added checkout and Payment functionality using stripe API, works now only for testing

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'search',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -139,6 +140,9 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 # Fix issue for cloud9, porbably irrelevant here
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
